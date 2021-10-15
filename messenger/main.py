049ffa35d9570450a99d9bf491f4973f8f1495c1
Marlin Jayasekera <marlinjayasekera@gmail.com>
2021-10-15 02:19:42 -0400

    move to 'microservices' + get goals roughly working

---
+++ messenger/main.py
@@ -0,0 +1,85 @@
+import asyncio
+import datetime
+import os
+import logging
+import sys
+import traceback
+from dotenv import load_dotenv
+from nio import AsyncClient, MatrixRoom, RoomMessageText
+
+load_dotenv()
+PROG_WITHOUT_EXT = os.path.splitext(os.path.basename(__file__))[0]
+
+
+async def main() -> None:
+    logger = logging.getLogger(PROG_WITHOUT_EXT)
+    client = AsyncClient("https://matrix.org", os.environ.get("MATRIX_USER"))
+    print(await client.login(os.environ.get("MATRIX_PASSWORD")))
+
+    callbacks = Callbacks(client)
+
+    client.add_event_callback(callbacks.message_callback, RoomMessageText)
+
+    sync_response = await client.sync_forever(timeout=30000)  # milliseconds
+
+
+async def message_callback(room: MatrixRoom, event: RoomMessageText) -> None:
+    print(
+        f"Message received in room {room.display_name}\n"
+        f"{room.user_name(event.sender)} | {event.body}"
+    )
+
+
+class Callbacks(object):
+    """Class to pass client to callback methods."""
+
+    def __init__(self, client: AsyncClient):
+        """Store AsyncClient."""
+        self.client = client
+
+    async def message_callback(self, room: MatrixRoom, event):
+        """
+        Handle all events of type RoomMessage.
+        Includes events like RoomMessageText, RoomMessageImage, etc.
+        """
+        try:
+            logger.debug(
+                f"message_callback(): for room {room} received this "
+                f"event: type: {type(event)}, event_id: {event.event_id}, "
+                f"event: {event}"
+            )
+            logger.debug(f"event.server_timestamp = {event.server_timestamp}")
+            timestamp = datetime.datetime.fromtimestamp(
+                int(event.server_timestamp / 1000)
+            )  # sec since 1970
+            event_datetime = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            # e.g. 2020-08-06 17:30:18
+            logger.debug(f"event_datetime = {event_datetime}")
+
+            print(
+                f"Message received in room {room.display_name}\n"
+                f"{room.user_name(event.sender)} | {event.body}"
+            )
+
+            if event.sender == "@itsmaleen:matrix.org":
+                if event.body == "1":
+                    await self.client.room_send(
+                        # Watch out! If you join an old room you'll see lots of old messages
+                        room_id=room.room_id,
+                        message_type="m.room.message",
+                        content={
+                            "msgtype": "m.text",
+                            "body": "test"
+                        }
+                    )
+                    print("done")
+
+
+        except BaseException:
+            logger.debug(traceback.format_exc())
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(PROG_WITHOUT_EXT)
+    asyncio.run(main())
+    sys.exit(1)
--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,27 +1,32 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from datetime import datetime as dt
+import requests
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+# Need to run `rasa run actions` concurrently
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionListGoals(Action):
+
+    def name(self) -> Text:
+        return "action_list_goals"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        time = tracker.get_slot("time")
+        time_object = dt.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
+        time_object.date()
+        response = requests.get('http://localhost:5000/goals',
+                                data={'date': time_object.strftime("%Y-%m-%d")})
+
+        print(response.status_code)
+        if response.status_code != 200:
+            dispatcher.utter_message(text="Sorry, something went wrong")
+            return []
+
+        dispatcher.utter_message(text=response.text)
+
+        return []
